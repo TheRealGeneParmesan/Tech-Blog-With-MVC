2297fa0fb7aff05d6caad06f20e992ee08759014
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-03-28 20:50:10 -0700

    added edit.js for frontend

---
+++ public/js/edit.js
@@ -0,0 +1,30 @@
+const editHandler = async (event) => {
+
+    event.preventDefault();
+
+    const name = document.querySelector('#blog-name').value.trim();
+    const description = document.querySelector('#blog-desc').value.trim();
+    const id = document.querySelector('#edit-btn').getAttribute('data-id');
+
+    alert(`name: ${name}, description: ${description}, id: ${id}`);
+
+    if (name && description) {
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ name, description }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to update blog');
+        }
+    }
+};
+
+document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', editHandler);
